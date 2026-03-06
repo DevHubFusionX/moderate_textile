ac331fd4d9e6c40d36e807c72d6c49223194cfc8
Franklin Fanya <franklinfanya@gmail.com>
2026-03-06 18:35:07 +0100

    text and images adkustment

--- src/components/ComboDetails.jsx
+++ src/components/ComboDetails.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaWhatsapp, FaChevronLeft, FaChevronRight, FaStar } from 'react-icons/fa';
 import { Helmet } from 'react-helmet-async';
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaWhatsapp, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { Helmet } from 'react-helmet-async';
